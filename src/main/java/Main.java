a564aa711bd640c0de1ec6f165611fe655244e08
Chessman97 <mikhailef@mail.ru>
2019-05-17 23:20:08 +0700

    Определение положения глаз

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,11 +25,29 @@ public class Main {
             System.out.println("Не удалось загрузить классификатор " + name);
             return;
         }
+        CascadeClassifier eye_detector = new CascadeClassifier();
+        name = "haarcascade_eye.xml";
+        System.out.println(path + name);
+        if (!eye_detector.load(path + name)) {
+            System.out.println("Не удалось загрузить классификатор " + name);
+            return;
+        }
 
         MatOfRect faces = new MatOfRect();
         face_detector.detectMultiScale(img, faces);
-        for (Rect r: faces.toList()) {
-            System.out.println(111);
+        for (Rect r : faces.toList()) {
+            Mat face = img.submat(r);
+            MatOfRect eyes = new MatOfRect();
+            eye_detector.detectMultiScale(face, eyes);
+
+            int count = 0;
+            for (Rect r2 : eyes.toList()) {
+                if (count > 1) {
+                    break;
+                }
+                count++;
+                Imgproc.rectangle(face, new Point(r2.x, r2.y), new Point(r2.x + r2.width, r2.y + r2.height), new Scalar(1, 1, 1, 1), 1);
+            }
             Imgproc.rectangle(img, new Point(r.x, r.y), new Point(r.x + r.width, r.y + r.height), new Scalar(1, 1, 1, 1), 2);
         }
 
@@ -47,12 +65,12 @@ public class Main {
         frame.setVisible(true);
     }
 
-    public static BufferedImage MatToBufferedImage(Mat m) {
+    private static BufferedImage MatToBufferedImage(Mat m) {
         if (m == null || m.empty()) {
             return null;
         }
         if (m.depth() == CvType.CV_8U) {
-
+            System.out.println("Ничего не надо делать");
         } else if (m.depth() == CvType.CV_16U) {
             Mat m_16 = new Mat();
             m.convertTo(m_16, CvType.CV_16U, 255.0 / 65535);
@@ -64,7 +82,7 @@ public class Main {
         } else {
             return null;
         }
-        int type = 0;
+        int type;
         if (m.channels() == 1) {
             type = BufferedImage.TYPE_BYTE_GRAY;
         } else if (m.channels() == 3) {
@@ -76,7 +94,7 @@ public class Main {
         }
         byte[] buf = new byte[m.channels() * m.cols() * m.rows()];
         m.get(0, 0, buf);
-        byte tmp = 0;
+        byte tmp;
         if (m.channels() == 4) {
             for (int i = 0; i < buf.length; i += 4) {
                 tmp = buf[i + 3];
