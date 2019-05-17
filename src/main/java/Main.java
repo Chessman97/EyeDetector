e0a1b58e5cfbdae0ba8d09f8bae6e0764b401b24
Chessman97 <mikhailef@mail.ru>
2019-05-18 02:56:58 +0700

    Определение стороны взгляда

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     private static Window window = new Window();
     private static Detector detector = new Detector();
-    private static Camera camera = new Camera(1, 800, 600);
+    private static Camera camera = new Camera(1, 640, 480);
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -23,7 +23,21 @@ public class Main {
             Imgproc.medianBlur(img, img, 5);
             Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);
 
-            detector.work(img);
+            int[] points = detector.work(img);
+
+            int xEye = points[0];
+            int yEye = points[1];
+            int xPoint = points[2];
+            int yPoint = points[3];
+
+//            System.out.println(xPoint + "  " + xEye);
+            if (xPoint - xEye > 1) {
+                System.out.println("left");
+            } else if (xEye - xPoint > 1) {
+                System.out.println("right");
+            } else {
+                System.out.println("GOVNO");
+            }
 
             window.show(img);
         }
--- src/main/java/model/Detector.java
+++ src/main/java/model/Detector.java
@@ -5,7 +5,6 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Detector {
 
@@ -32,7 +31,16 @@ public class Detector {
         }
     }
 
-    public List<Mat> work(Mat img) {
+    public int[] work(Mat img) {
+        int xEye1 = 0;
+        int yEye1 = 0;
+        int xPoint1 = 0;
+        int yPoint1 = 0;
+        int xEye2 = 0;
+        int yEye2 = 0;
+        int xPoint2 = 0;
+        int yPoint2 = 0;
+
         MatOfRect faces = new MatOfRect();
         face_detector.detectMultiScale(img, faces);
         for (Rect r : faces.toList()) {
@@ -52,21 +60,41 @@ public class Detector {
                         }
                     }
 //                    Imgproc.rectangle(face, new Point(r2.x, r2.y), new Point(r2.x + r2.width, r2.y + r2.height), new Scalar(1, 1, 1, 1), 2);
+//                    Imgproc.line(face, new Point(r2.x, r2.y), new Point(r2.x + r2.width, r2.y + r2.height), new Scalar(1, 1, 1, 1), 2);
+//                    Imgproc.line(face, new Point(r2.x + r2.width, r2.y), new Point(r2.x, r2.y + r2.height), new Scalar(1, 1, 1, 1), 2);
 
                     Imgproc.Canny(eye, eye, 80, 200);
                     ArrayList<MatOfPoint> contours = new ArrayList<>();
                     Imgproc.findContours(eye, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-                    System.out.println(contours.size());
                     for (MatOfPoint contour : contours) {
                         Rect r3 = Imgproc.boundingRect(contour);
-                        if (r3.width > 20) { // TODO
-                            Imgproc.rectangle(eye, new Point(r3.x, r3.y), new Point(r3.x + r3.width - 1, r3.y + r3.height - 1), new Scalar(255, 0, 0, 255), 1);
+                        if (r3.width > 10) { // TODO
+                            if (xEye1 == 0) {
+                                xEye1 = r2.width / 2;
+                                yEye1 = r2.height / 2;
+                                xPoint1 = r3.x + r3.width / 2;
+                                yPoint1 = r3.y + r3.height / 2;
+//                                Imgproc.rectangle(eye, new Point(xEye, yEye), new Point(xEye + 1, yEye + 1), new Scalar(1, 1, 1, 1), 3);
+                            } else if (xEye2 == 0) {
+                                xEye2 = r2.width / 2;
+                                yEye2 = r2.height / 2;
+                                xPoint2 = r3.x + r3.width / 2;
+                                yPoint2 = r3.y + r3.height / 2;
+                            }
+//                            Imgproc.line(eye, new Point(xEye, 0), new Point(xEye, 600), new Scalar(255, 255, 255, 255), 1);
+//                            Imgproc.line(eye, new Point(xPoint, 0), new Point(xPoint, 600), new Scalar(255, 255, 255, 255), 1);
+
+//                            Imgproc.rectangle(eye, new Point(r3.x, r3.y), new Point(r3.x + r3.width - 1, r3.y + r3.height - 1), new Scalar(255, 255, 255, 255), 1);
+//                            Imgproc.line(eye, new Point(r3.x, r3.y), new Point(r3.x + r3.width - 1, r3.y + r3.height - 1), new Scalar(255, 255, 255, 255), 1);
+//                            Imgproc.line(eye, new Point(r3.x + r3.width - 1, r3.y), new Point(r3.x, r3.y + r3.height - 1), new Scalar(255, 255, 255, 255), 1);
                         }
                     }
                 }
             }
 //            Imgproc.rectangle(img, new Point(r.x, r.y), new Point(r.x + r.width, r.y + r.height), new Scalar(1, 1, 1, 1), 2);
+//            Imgproc.line(img, new Point(r.x, r.y), new Point(r.x + r.width, r.y + r.height), new Scalar(1, 1, 1, 1), 2);
+//            Imgproc.line(img, new Point(r.x + r.width, r.y), new Point(r.x, r.y + r.height), new Scalar(1, 1, 1, 1), 2);
         }
-        return null;
+        return new int[]{xEye1, yEye1, xPoint1, yPoint1, xEye2, yEye2, xPoint2, yPoint2};
     }
 }
