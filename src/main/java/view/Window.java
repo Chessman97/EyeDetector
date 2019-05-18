13a3a0df42d8788b87f353f139ccc2318d90f4f8
Chessman97 <mikhailef@mail.ru>
2019-05-18 23:17:06 +0700

    Анимация волка

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -14,8 +14,8 @@ public class Window extends JFrame {
     private ImageIcon imageIconWolf1 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\wolf1.png");
     private ImageIcon imageIconWolf2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\wolf2.png");
     private ImageIcon hand1 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand1.png");
-    private ImageIcon hand2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand2.png");
-    private ImageIcon hand3 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand3.png");
+    private ImageIcon hand2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand3.png");
+    private ImageIcon hand3 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand2.png");
     private ImageIcon hand4 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand4.png");
     private ImageIcon egg1 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg1.png");
     private ImageIcon egg2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg2.png");
@@ -42,29 +42,9 @@ public class Window extends JFrame {
         game = true;
     }
 
-    private void setWolf(ImageIcon imageIcon) {
-        getGraphics().drawImage(imageIcon.getImage(), 300, 0,null);
-    }
-
     private void setImage(ImageIcon imageIcon, int n) {
-        int x = 0;
-        int y = 0;
-        if (n == 1) {
-            x = 0;
-            y = 0;
-        }
-        if (n == 2) {
-            x = 1366 - 770;
-            y = 0;
-        }
-        if (n == 3) {
-            x = 0;
-            y = 700 - 330;
-        }
-        if (n == 4) {
-            x = 1300 - 460;
-            y = 700 - 330;
-        }
+        int x = 1366 - 850;
+        int y = 240;
         getGraphics().drawImage(imageIcon.getImage(), x, y,null);
     }
 
@@ -79,10 +59,46 @@ public class Window extends JFrame {
             g2.drawLine(1066, 270, 1390, 100);
             g2.drawLine(1066, 520, 1390, 350);
         }
-        if(state == 1 || state == 3)
+        if (state == 1 || state == 3) {
             setImage(imageIconWolf1, state);
-        if(state == 2 || state == 4)
+            if (state == 1) {
+                setImageHand(hand1, state);
+            }
+            else {
+                setImageHand(hand3, state);
+            }
+        }
+        if (state == 2 || state == 4) {
             setImage(imageIconWolf2, state);
+            if (state == 2){
+                setImageHand(hand2, state);
+            }
+            else{
+                setImageHand(hand4, state);
+            }
+        }
+    }
+
+    private void setImageHand(ImageIcon imageIcon, int n) {
+        int x = 0;
+        int y = 0;
+        if (n == 1) {
+            x = 300;
+            y = 270;
+        }
+        if (n == 2) {
+            x = 1066 - 260;
+            y = 270;
+        }
+        if (n == 3) {
+            x = 300;
+            y = 450;
+        }
+        if (n == 4) {
+            x = 1066 - 260;
+            y = 450;
+        }
+        getGraphics().drawImage(imageIcon.getImage(), x, y,null);
     }
 
 }
