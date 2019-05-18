ea0773d209934b17358a1f6cb87e51e5c4e55dbd
FedorovVladimir <vladimir.fodorow@gmail.com>
2019-05-18 15:45:09 +0700

    Картинки волка выводятся по заданому состоянию

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -2,13 +2,19 @@ package view;
 
 import org.opencv.core.Mat;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Window extends JFrame {
 
     public Window() {
-        setTitle("Simple GUI");
+        setTitle("EggCatcher");
         setSize(800, 600);
+        setExtendedState(Frame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -21,5 +27,38 @@ public class Window extends JFrame {
         JScrollPane pane = new JScrollPane(label);
         setContentPane(pane);
         setVisible(true);
+    }
+
+    public void setState(int numberState) {
+        if (numberState == 1 || numberState == 3) {
+            setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/wolf1.png");
+            if (numberState == 1) {
+                setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/hand1.png");
+            } else {
+                setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/hand3.png");
+            }
+        }
+        if (numberState == 2 || numberState == 4) {
+            setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/wolf2.png");
+            if (numberState == 2) {
+                setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/hand2.png");
+            } else {
+                setImage("/home/vladimir/IdeaProjects/EggsCatcher/src/main/resources/img/hand4.png");
+            }
+        }
+    }
+
+    private void setImage(String path) {
+        try {
+            BufferedImage bi = ImageIO.read(new File(path));
+            ImageIcon imageIcon = new ImageIcon(bi);
+            JLabel label = new JLabel(imageIcon);
+            label.setDisabledIcon(imageIcon);
+            JScrollPane pane = new JScrollPane(label);
+            setContentPane(pane);
+            setVisible(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
