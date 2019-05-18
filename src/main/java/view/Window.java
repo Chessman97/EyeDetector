1e95b2b7992d28d30f8542a4bf674d01025675f4
Chessman97 <mikhailef@mail.ru>
2019-05-18 23:52:42 +0700

    Первое яйцо

--- src/main/java/view/Window.java
+++ src/main/java/view/Window.java
@@ -2,14 +2,12 @@ package view;
 
 import org.opencv.core.Mat;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Window extends JFrame {
+
+    private boolean egg1Visible = true;
 
     private ImageIcon imageIconWolf1 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\wolf1.png");
     private ImageIcon imageIconWolf2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\wolf2.png");
@@ -17,11 +15,15 @@ public class Window extends JFrame {
     private ImageIcon hand2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand3.png");
     private ImageIcon hand3 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand2.png");
     private ImageIcon hand4 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\hand4.png");
+    private ImageIcon egg = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg.png");
     private ImageIcon egg1 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg1.png");
     private ImageIcon egg2 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg2.png");
     private ImageIcon egg3 = new ImageIcon("C:\\Users\\Mikhail\\IdeaProjects\\EggsCatcherPath2\\src\\main\\resources\\img\\egg3.png");
     private int state;
     private boolean game;
+
+    private double xEgg = -60;
+    private double yEgg = -20;
 
     public Window() {
         setTitle("EggCatcher");
@@ -54,7 +56,7 @@ public class Window extends JFrame {
         Graphics2D g2 = (Graphics2D) g;
         if (game) {
             g2.setStroke(new BasicStroke(10.0f));  // толщина равна 10
-            g2.drawLine(0, 100, 300, 270);
+            g2.drawLine(0, 100, 300, 200);
             g2.drawLine(0, 350, 300, 520);
             g2.drawLine(1066, 270, 1390, 100);
             g2.drawLine(1066, 520, 1390, 350);
@@ -76,6 +78,19 @@ public class Window extends JFrame {
             else{
                 setImageHand(hand4, state);
             }
+        }
+        if (xEgg < 300) {
+            xEgg += 9;
+            yEgg += 3;
+        } else {
+            yEgg += 9;
+            if (state == 1 && yEgg > 290) {
+                egg1Visible = false;
+                System.out.println("Вы поймали яйцо!");
+            }
+        }
+        if (egg1Visible) {
+            getGraphics().drawImage(egg.getImage(), (int) xEgg, (int) yEgg + 30, null);
         }
     }
 
